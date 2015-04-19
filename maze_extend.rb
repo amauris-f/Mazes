e8ef4d327f4b0f366419832372d77f3def849855
Amauris <amfer17@brandeis.edu>
2015-04-19 02:29:51 -0400

    Mazes

--- maze_extend.rb
+++ maze_extend.rb
@@ -1,123 +1,123 @@
-#This file contains extended methods of the Maze class, as well as a Point class
-#which represents cell positions in the maze
-class Maze
-	@visited
-	@all_paths
+# #This file contains extended methods of the Maze class, as well as a Point class
+# #which represents cell positions in the maze
+# class Maze
+# 	@visited
+# 	@all_paths
 
-	def solve(params = {})
-		check = @maze_check.loaded?
-		if check != true then return check end
-		result = traverse(params)
-		if result
-			puts "There is a path between these two points. Use trace to look at the steps."
-		else
-			puts "Unsolvable: There is no path between these two points."
-		end
-	end
+# 	def solve(params = {})
+# 		check = @maze_check.loaded?
+# 		if check != true then return check end
+# 		result = traverse(params)
+# 		if result
+# 			puts "There is a path between these two points. Use trace to look at the steps."
+# 		else
+# 			puts "Unsolvable: There is no path between these two points."
+# 		end
+# 	end
 
-	def trace(params = {})
-		check = @maze_check.loaded?
-		if check != true then return check end
-		traceable = traverse(params)
-		if traceable
-			steps_taken
-		else
-			puts "Untraceable: There is no path between these two points."
-		end
-	end
-	private
-	def steps_taken
-		mvmnt = ["Right", "Down", "Left", "Up"]
-		stack = Array.new()
-		add_to_stack(stack)
-		(stack.size).times do |step|
-			cell = stack.pop()
-			puts "#{step+1}. From cell: #{cell}, Move #{mvmnt[cell.dir]} to #{cell.next}."
-		end
-	end
+# 	def trace(params = {})
+# 		check = @maze_check.loaded?
+# 		if check != true then return check end
+# 		traceable = traverse(params)
+# 		if traceable
+# 			steps_taken
+# 		else
+# 			puts "Untraceable: There is no path between these two points."
+# 		end
+# 	end
+# 	private
+# 	def steps_taken
+# 		mvmnt = ["Right", "Down", "Left", "Up"]
+# 		stack = Array.new()
+# 		add_to_stack(stack)
+# 		(stack.size).times do |step|
+# 			cell = stack.pop()
+# 			puts "#{step+1}. From cell: #{cell}, Move #{mvmnt[cell.dir]} to #{cell.next}."
+# 		end
+# 	end
 
-	def add_to_stack(stack)
-		transition = @all_paths.pop()
-		while transition.prev_step
-				transition = transition.prev_step
-				stack.push(transition)
-		end
-	end
+# 	def add_to_stack(stack)
+# 		transition = @all_paths.pop()
+# 		while transition.prev_step
+# 				transition = transition.prev_step
+# 				stack.push(transition)
+# 		end
+# 	end
 
-	def traverse(params)
-		beg_point = Point.new(params[:begX], params[:begY])
-		end_point = Point.new(params[:endX], params[:endY])
-		@all_paths = Array.new()
-		@visited = Array.new()
-		@all_paths.push(beg_point)
-		while !@all_paths.include?(end_point)
-			if @all_paths.empty? then return false end
-			trans = @all_paths.shift()
-			if !@visited.include?(trans) then one_step_moves(trans) end
-		end
-		return true
-	end
+# 	def traverse(params)
+# 		beg_point = Point.new(params[:begX], params[:begY])
+# 		end_point = Point.new(params[:endX], params[:endY])
+# 		@all_paths = Array.new()
+# 		@visited = Array.new()
+# 		@all_paths.push(beg_point)
+# 		while !@all_paths.include?(end_point)
+# 			if @all_paths.empty? then return false end
+# 			trans = @all_paths.shift()
+# 			if !@visited.include?(trans) then one_step_moves(trans) end
+# 		end
+# 		return true
+# 	end
 
-	def one_step_moves(trans)
-		@visited.push(trans)
-		(0..3).each do |dir|
-			if valid_move?(trans, dir)
-				add_position(trans, dir)
-			end
-		end
-	end
+# 	def one_step_moves(trans)
+# 		@visited.push(trans)
+# 		(0..3).each do |dir|
+# 			if valid_move?(trans, dir)
+# 				add_position(trans, dir)
+# 			end
+# 		end
+# 	end
 
-	def add_position(trans, dir)
-		x_move = [1, 0, -1, 0]
-		y_move = [0, 1, 0, -1]
-		new_x = trans.x + x_move[dir]
-		new_y = trans.y + y_move[dir]
-		new_pos = Point.new(new_x, new_y)
-		marked?(trans, dir, new_pos)
-	end
+# 	def add_position(trans, dir)
+# 		x_move = [1, 0, -1, 0]
+# 		y_move = [0, 1, 0, -1]
+# 		new_x = trans.x + x_move[dir]
+# 		new_y = trans.y + y_move[dir]
+# 		new_pos = Point.new(new_x, new_y)
+# 		marked?(trans, dir, new_pos)
+# 	end
 
-	def marked?(trans, dir, new_pos)
-		if !@visited.include?(new_pos)
-			new_pos.prev_step = trans
-			trans.next = new_pos
-			trans.dir = dir
-			@all_paths.push(new_pos)
-		end
-	end
+# 	def marked?(trans, dir, new_pos)
+# 		if !@visited.include?(new_pos)
+# 			new_pos.prev_step = trans
+# 			trans.next = new_pos
+# 			trans.dir = dir
+# 			@all_paths.push(new_pos)
+# 		end
+# 	end
 
-	def valid_move?(trans, dir)
-			col_ary = trans.x * 2 + 1
-			row_ary = trans.y * 2 + 1
-			case dir
-			when 0
-				if maze_a.fetch(row_ary)[col_ary + 1] == "0" then return true end
-			when 1
-				if maze_a.fetch(row_ary + 1)[col_ary] == "0" then return true end
-			when 2
-				if maze_a.fetch(row_ary)[col_ary - 1] == "0" then return true end
-			when 3
-				if maze_a.fetch(row_ary - 1)[col_ary] == "0" then return true end
-			else
-				return false
-			end
-	end
-end
+# 	def valid_move?(trans, dir)
+# 			col_ary = trans.x * 2 + 1
+# 			row_ary = trans.y * 2 + 1
+# 			case dir
+# 			when 0
+# 				if maze_a.fetch(row_ary)[col_ary + 1] == "0" then return true end
+# 			when 1
+# 				if maze_a.fetch(row_ary + 1)[col_ary] == "0" then return true end
+# 			when 2
+# 				if maze_a.fetch(row_ary)[col_ary - 1] == "0" then return true end
+# 			when 3
+# 				if maze_a.fetch(row_ary - 1)[col_ary] == "0" then return true end
+# 			else
+# 				return false
+# 			end
+# 	end
+# end
 
 
-class Point
-	attr_reader :x, :y
-	attr_accessor :prev_step, :dir, :next
+# class Point
+# 	attr_reader :x, :y
+# 	attr_accessor :prev_step, :dir, :next
 
-	def initialize(x, y)
-		@x = x
-		@y = y
-	end
+# 	def initialize(x, y)
+# 		@x = x
+# 		@y = y
+# 	end
 
-	def ==(other_point)
-		self.x == other_point.x && self.y == other_point.y
-	end
+# 	def ==(other_point)
+# 		self.x == other_point.x && self.y == other_point.y
+# 	end
 
-	def to_s
-			return "(#{x} , #{y})"
-	end
-end
+# 	def to_s
+# 			return "(#{x} , #{y})"
+# 	end
+# end
--- mazes.rb
+++ mazes.rb
@@ -8,6 +8,8 @@ class Maze
 	@maze_s = nil
 	@maze_a
 	@maze_display
+	@visited
+	@all_paths
 
 	def initialize(columns, rows)
 		@columns = columns
@@ -41,7 +43,6 @@ class Maze
 		draw_borders
 	end
 
-	private
 	def convert_maze_to_a
 		position = 0
 		(0...length).each do |row_ind|
@@ -78,6 +79,103 @@ class Maze
 			end
 			@maze_display += "\n"
 		end
+	end
+		def solve(params = {})
+		check = @maze_check.loaded?
+		if check != true then return check end
+		result = traverse(params)
+		if result
+			puts "There is a path between these two points. Use trace to look at the steps."
+		else
+			puts "Unsolvable: There is no path between these two points."
+		end
+	end
+
+	def trace(params = {})
+		check = @maze_check.loaded?
+		if check != true then return check end
+		traceable = traverse(params)
+		if traceable
+			steps_taken
+		else
+			puts "Untraceable: There is no path between these two points."
+		end
+	end
+	private
+	def steps_taken
+		mvmnt = ["Right", "Down", "Left", "Up"]
+		stack = Array.new()
+		add_to_stack(stack)
+		(stack.size).times do |step|
+			cell = stack.pop()
+			puts "#{step+1}. From cell: #{cell}, Move #{mvmnt[cell.dir]} to #{cell.next}."
+		end
+	end
+
+	def add_to_stack(stack)
+		transition = @all_paths.pop()
+		while transition.prev_step
+				transition = transition.prev_step
+				stack.push(transition)
+		end
+	end
+
+	def traverse(params)
+		beg_point = Point.new(params[:begX], params[:begY])
+		end_point = Point.new(params[:endX], params[:endY])
+		@all_paths = Array.new()
+		@visited = Array.new()
+		@all_paths.push(beg_point)
+		while !@all_paths.include?(end_point)
+			if @all_paths.empty? then return false end
+			trans = @all_paths.shift()
+			if !@visited.include?(trans) then one_step_moves(trans) end
+		end
+		return true
+	end
+
+	def one_step_moves(trans)
+		@visited.push(trans)
+		(0..3).each do |dir|
+			if valid_move?(trans, dir)
+				add_position(trans, dir)
+			end
+		end
+	end
+
+	def add_position(trans, dir)
+		x_move = [1, 0, -1, 0]
+		y_move = [0, 1, 0, -1]
+		new_x = trans.x + x_move[dir]
+		new_y = trans.y + y_move[dir]
+		new_pos = Point.new(new_x, new_y)
+		marked?(trans, dir, new_pos)
+	end
+
+	def marked?(trans, dir, new_pos)
+		if !@visited.include?(new_pos)
+			new_pos.prev_step = trans
+			trans.next = new_pos
+			trans.dir = dir
+			@all_paths.push(new_pos)
+		end
+	end
+
+	def valid_move?(trans, dir)
+			col_ary = trans.x * 2 + 1
+			row_ary = trans.y * 2 + 1
+			case dir
+			when 0
+				if maze_a.fetch(row_ary)[col_ary + 1] == "0" then return true end
+			when 1
+				if maze_a.fetch(row_ary + 1)[col_ary] == "0" then return true end
+			when 2
+				if maze_a.fetch(row_ary)[col_ary - 1] == "0" then return true end
+			when 3
+				if maze_a.fetch(row_ary - 1)[col_ary] == "0" then return true end
+			else
+				return false
+			end
 	end
 end
 
@@ -169,6 +267,125 @@ because it contains either 0 or 4 walls. Load unsuccessful."""
 		else
 			return true
 		end
+	end
+
+	def redesign()
+		@maze_a = Array.new(length){|i| Array.new(width) { |i| "1"}}
+		reset_cells
+		create_walls
+		check_for_invalid
+		draw_maze
+	end
+	private
+	def reset_cells
+		(1...length - 1).each do |row|
+			(1...width - 1).each do |col|
+				if row.even? && col.even?
+					maze_a.fetch(row)[col] = "1"
+				else
+					maze_a.fetch(row)[col] = "0"
+				end
+			end
+		end
+	end
+
+	def create_walls
+		(1...length - 1).step(2) do |row|
+			(1...width).step(2) do |col|
+				spaces = adj_walls(row, col)
+				choose_sides(spaces, row, col)
+			end
+		end
+	end
+
+	def check_for_invalid
+		(1...length - 1).step(2) do |row|
+			(1...width).step(2) do |col|
+				spaces = adj_walls(row, col)
+				if spaces.empty?
+					walls = non_borders(row, col)
+					construct(row, col, walls[rand(walls.length)], "0")
+				end
+			end
+		end
+	end
+
+	def non_borders(row, col) 
+		walls = [0, 1, 2, 3]
+		if row + 1 == width
+			walls.delete_if{|x, y| y == 2}
+		elsif row - 1 == 0
+			walls.delete_if{|x, y| y == 0}
+		end
+		if col - 1 == 0
+			walls.delete_if{|x, y| y == 3}
+		elsif col + 1 == length
+			walls.delete_if{|x, y| y == 1}
+		end
+		return walls
+	end
+
+	def adj_walls(row, col)
+		wall = Array.new()
+		sides = [0, 1, 2, 3]
+		wall = find_walls(row, col, 1, 1, wall)
+		wall = find_walls(row, col, -1, 0, wall)
+		spaces_left = sides - wall
+		return spaces_left
+	end
+
+	def find_walls(row, col, int, dir, wall)
+		x_val = 3 - 2 * (dir)
+		y_val = 2 * dir
+		if maze_a.fetch(row + int)[col] == "1" then wall.push(y_val) end
+		if maze_a.fetch(row)[col + int] == "1" then wall.push(x_val) end
+		return wall
+	end
+
+	def choose_sides(spaces, row, col)
+		if spaces.empty?
+			choose_side = rand(0..3)
+			construct(row,col, choose_side, "0")
+		else
+			(0...3).each do |mult|
+				prob = (3 - mult) * 3
+				if spaces.size > 2 && rand(0...10) >= prob
+					choose_side = spaces.delete_at(rand(spaces.length))
+					construct(row, col, choose_side, "1")
+				end
+			end
+		end
+	end
+
+	def construct(row, col, choose_side, w_or_s)
+		case choose_side
+		when 0
+			maze_a.fetch(row - 1)[col] = w_or_s
+		when 1
+			maze_a.fetch(row)[col + 1] = w_or_s
+		when 2
+			maze_a.fetch(row + 1)[col] = w_or_s
+		else
+			maze_a.fetch(row)[col - 1] = w_or_s
+		end
+	end
+end
+
+class Point
+	attr_reader :x, :y
+	attr_accessor :prev_step, :dir, :next
+
+	def initialize(x, y)
+		@x = x
+		@y = y
+	end
+
+	def ==(other_point)
+		self.x == other_point.x && self.y == other_point.y
+	end
+
+	def to_s
+			return "(#{x} , #{y})"
 	end
 end
 
